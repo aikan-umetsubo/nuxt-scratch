64bd666d75a6144ab735bbd04b170fb4e6ff77cf
aikan-umetsubo <yakinori21@gmail.com>
2020-08-29 13:52:35 +0900

    nuxt.config.jsでグローバルに使われるhead要素を設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,5 +14,20 @@ export default {
 
   proxy: {
     '/api/':  'http://localhost:3000'
+  },
+
+  head: {
+    title: 'スクラッチで作ったToDoアプリ',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Nuxt.jsとRuby on Railsの勉強のために作ったWebアプリ'
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   }
+
 }
